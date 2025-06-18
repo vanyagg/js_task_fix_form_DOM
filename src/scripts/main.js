2f6808959deb0e2327503a99af10bb740c22c3fb
Vanya Holovey <golovejvana9@gmail.com>
2025-06-18 21:41:43 +0300

    auth form fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const inputName = input.getAttribute('name');
+  const inputNameCapitalized = inputName[0].toUpperCase()
+  + inputName.slice(1);
+  const inputId = input.getAttribute('id');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', inputId);
+  input.setAttribute('placeholder', inputNameCapitalized);
+  label.textContent = inputNameCapitalized;
+  input.insertAdjacentElement("beforebegin", label);
+}
